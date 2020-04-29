e27f920317db0875b9176f9f1ac5a0f51df63965
AndrewC7 <a.cox@me.com>
2020-04-29 21:08:45 +0000

    Remove comma from end

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for tens in range(0, 10):
     for ones in range(tens + 1, 10):
-        print("{}{}".format(tens, ones), end=", ")
+        if tens == 8 and ones == 9:
+            print(("{}{}".format(tens, ones)))
+        else:
+            print("{}{}".format(tens, ones), end=", ")
